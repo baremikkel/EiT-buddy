34b58a0067cae4f971ccbce3cade1b0b2a2bf89c
baremikkel <75213352+baremikkel@users.noreply.github.com>
2023-11-02 15:39:29 +0100

    Added get options

--- src/main/java/com/example/eitbuddy/Database.java
+++ src/main/java/com/example/eitbuddy/Database.java
@@ -1,6 +1,7 @@
 package com.example.eitbuddy;
 
 import java.sql.*;
+
 import io.github.cdimascio.dotenv.Dotenv;
 
 public class Database {
@@ -11,7 +12,7 @@ public class Database {
     private final String user = dotenv.get("DATABASE_USERNAME");
     private final String password = dotenv.get("DATABASE_PASSWORD");
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         try {
             Database database = getConnection();
             //database.insertUser("a","s","d");
@@ -19,6 +20,7 @@ public class Database {
             throw new RuntimeException(e);
         }
     }
+
     public Database() {
         try {
             connection = DriverManager.getConnection(url, user, password);
@@ -30,11 +32,12 @@ public class Database {
 
     public void addNewUser(String name, String mail, String pass) throws SQLException {
         PreparedStatement insert = connection.prepareStatement("INSERT INTO Users(name, email, password) VALUES (?,?,?)");
-        insert.setString(1,name);
-        insert.setString(2,mail);
-        insert.setString(3,pass);
+        insert.setString(1, name);
+        insert.setString(2, mail);
+        insert.setString(3, pass);
         insert.executeUpdate();
     }
+
     public void addBuddy(int userId, String planttype) throws SQLException {
         PreparedStatement insert = connection.prepareStatement("INSERT INTO buddies(userid, planttype) VALUES (?,?)");
         insert.setInt(1, userId);
@@ -43,14 +46,41 @@ public class Database {
 
     public void insertData(int buddyId, double temperature, double light, double humidity) throws SQLException {
         PreparedStatement insert = connection.prepareStatement("INSERT INTO sensorvalues(buddyid, temperature, light, soil) VALUES (?,?,?,?)");
-        insert.setInt(1,buddyId);
-        insert.setDouble(2,temperature);
-        insert.setDouble(3,light);
-        insert.setDouble(4,humidity);
+        insert.setInt(1, buddyId);
+        insert.setDouble(2, temperature);
+        insert.setDouble(3, light);
+        insert.setDouble(4, humidity);
+    }
+    public ResultSet collectData(int buddy) throws SQLException {
+        ResultSet set = null;
+        try {
+            connection.createStatement().executeQuery("SELECT temperature,light,soil FROM sensorvalues WHERE buddyid = " + buddy);
+        } catch (SQLException e) {
+            e.getMessage();
+        }
+        return set;
+    }
+    public ResultSet getBuddies(int user) {
+        ResultSet set = null;
+        try {
+            connection.createStatement().executeQuery("SELECT planttype FROM buddies WHERE userid=" + user);
+        } catch (SQLException e ){
+            e.getMessage();
+        }
+        return set;
+    }
+    public ResultSet getUser(int user) {
+        ResultSet set = null;
+        try {
+            connection.createStatement().executeQuery("SELECT name, email FROM users WHERE id=" + user);
+        } catch (SQLException e ){
+            e.getMessage();
+        }
+        return set;
     }
 
     static public Database getConnection() throws SQLException {
-        if(db_connection == null)
+        if (db_connection == null)
             db_connection = new Database();
         return db_connection;
     }

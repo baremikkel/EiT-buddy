d793ec960456aec2583ea499f5e82f1c530e4a99
Mikkel Weinreich Olsen <mikkelo.privat@gmail.com>
2023-11-02 22:26:55 +0100

    Migrated to a better place
    Due to me not being able to integrate Spring Boot before i recreated the applicaiton with the spring boot wizard

---
+++ src/main/java/com/example/eitbuddy/EiTBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.example.eitbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EiTBuddyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EiTBuddyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/eitbuddy/EiTBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eitbuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EiTBuddyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

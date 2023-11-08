726998d50e5750b5728a3e6199051d282e378eb6
baremikkel <75213352+baremikkel@users.noreply.github.com>
2023-11-08 15:23:44 +0100

    :)

--- buddy-app/App.tsx
+++ buddy-app/App.tsx
@@ -1,13 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, StyleSheet, View } from 'react-native';
-import {Test} from './components/LoginScreen'
+import {Test} from './LoginScreen'
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.innerContainer}>
         <Test></Test>
-      </View>
     </SafeAreaView>
   );
 }
@@ -15,11 +13,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-  },
-  innerContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    padding: 15,
+    backgroundColor: '#fff'
   }
 });

bd537ed18b07d9592443d535d4e47a57acca9e70
baremikkel <75213352+baremikkel@users.noreply.github.com>
2023-11-09 16:07:03 +0100

    Revert "Merge branch 'buddy-app' of https://github.com/baremikkel/EiT-buddy into buddy-app"
    This reverts commit ca566bbea934355f33aa3dd0d7d3c5b9733ee7f5, reversing
    changes made to ef48c66654802b53355ff0aca7b672286118f801.

--- buddy-app/screens/Home.tsx
+++ buddy-app/screens/Home.tsx
@@ -1,18 +1,10 @@
 import React from 'react';
 import {TextInput, Text, View, StyleSheet, Image, Button, Pressable, ImageBackground} from 'react-native'
 
-const style = StyleSheet.create({
-    container: {
-        padding: 20,
-    },
-});
-
-const Homescreen: React.FC = () => {
+export const Homescreen: React.FC = () => {
     return(
-        <View style = {style.container}>
-            <Text>New Screeeeeeen</Text>
+        <View>
+            <Text>New Screen</Text>
         </View>
     );
 };
-
-export default Homescreen;

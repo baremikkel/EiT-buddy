1f23cc04a9538f67a5c6b67c313edeaaaf031259
Mikkel Weinreich Olsen <mikkelo.privat@gmail.com>
2023-11-30 00:06:11 +0100

    easy clap

--- buddy-app/screens/LoginScreen.tsx
+++ buddy-app/screens/LoginScreen.tsx
@@ -1,14 +1,14 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
-import {TextInput, Text, View, StyleSheet, Image, Button, Pressable, ImageBackground} from 'react-native'
-import {storeUser, getId, getUrl} from './storage/DataStorage'
+import { TextInput, Text, View, StyleSheet, Image, Button, Pressable, ImageBackground } from 'react-native'
+import { storeUser, getId, getUrl } from './storage/DataStorage'
 import { useNavigation } from '@react-navigation/native';
 
 export const Login = () => {
     const [isValidPassword, setIsValidPassword] = useState<boolean>(true);
     const [isRegistered, setIsRegistered] = useState<boolean>(true);
 
-    const navigation =useNavigation();
+    const navigation = useNavigation();
     const login = () => {
         navigation.navigate('Homescreen')
     }
@@ -22,22 +22,22 @@ export const Login = () => {
     const verify = (
         Email: string,
         Password: string
-         ) => {
-        axios.get(getUrl()+'/users')
-            .then((response) =>{
+    ) => {
+        axios.get(getUrl() + '/users')
+            .then((response) => {
                 setData(response.data)
-                response.data.forEach((user: {id: any; name: string; email: string; password: string; }) => {
-                    if(Email == user.email && Password == user.password) {
-                        storeUser(user.id, [user.name,user.email, user.password]);
+                response.data.forEach((user: { id: any; name: string; email: string; password: string; }) => {
+                    if (Email == user.email && Password == user.password) {
+                        storeUser(user.id, [user.name, user.email, user.password]);
                         setIsRegistered(true);
                         setIsValidPassword(true);
                         login();
-                    } 
-                    if(Email == user.email && Password != user.password) {
+                    }
+                    if (Email == user.email && Password != user.password) {
                         setIsRegistered(true);
                         setIsValidPassword(false);
-                    } 
-                    if(Email != user.email) {
+                    }
+                    if (Email != user.email) {
                         setIsRegistered(false);
                     }
                 })
@@ -46,12 +46,16 @@ export const Login = () => {
                 console.log(error)
             })
     }
-    
+
 
     return (
         <View style={styles.container}>
-            <ImageBackground source={require('buddy-app/textures/plantBackground.jpg')} resizeMode='cover' style={styles.image}>
-           <Text style={[styles.text, styles.textMod]}>Email</Text>
+
+            <View style={styles.image_container}>
+                <Image
+                    source={require('buddy-app/textures/logo.png')} style={styles.image} />
+            </View>
+            <Text style={[styles.textMod]}>Email</Text>
             <TextInput
                 autoCapitalize='none'
                 value={email}
@@ -62,11 +66,11 @@ export const Login = () => {
             ></TextInput>
 
             {!isRegistered && (
-                                <Text style={styles.warning}>No account with this email</Text>
-                              )}
+                <Text style={styles.warning}>No account with this email</Text>
+            )}
 
-            <Text style={[styles.text, styles.textMod]}>Password</Text>
-            <TextInput 
+            <Text style={[styles.textMod]}>Password</Text>
+            <TextInput
                 autoCapitalize='none'
                 value={password}
                 onChangeText={setPass}
@@ -77,17 +81,15 @@ export const Login = () => {
             ></TextInput>
 
             {!isValidPassword && (
-                    <Text style={styles.warning}>Invalid password!</Text>
-                  )}
+                <Text style={styles.warning}>Invalid password!</Text>
+            )}
 
             <Pressable style={styles.button} onPress={() => verify(email, password)}>
                 <Text style={styles.text}>Login</Text>
             </Pressable>
             <Pressable onPress={signup} style={styles.signup}>
-                <Text style={styles.text}>or sign up here</Text>
+                <Text>or sign up here</Text>
             </Pressable>
-            
-            </ImageBackground>
         </View>
     )
 }
@@ -96,21 +98,21 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center'
-      },
-    input:{
+    },
+    input: {
         marginHorizontal: 15,
         borderRadius: 20,
         borderWidth: 1,
         height: 40,
         paddingLeft: 20,
-        backgroundColor: 'rgba(0,0,0,0.4)',
-        color: 'white' 
+        backgroundColor: '#005691',
+        color: 'white'
     },
-    textMod:{
+    textMod: {
         paddingVertical: 15,
         paddingLeft: 20,
     },
-    image: {
+    background_image: {
         flex: 1,
         justifyContent: 'center'
     },
@@ -122,9 +124,9 @@ const styles = StyleSheet.create({
         marginVertical: 15,
         marginHorizontal: 50,
         alignItems: 'center',
-        backgroundColor: 'rgba(0,15,0,0.3)',
+        backgroundColor: '#005691',
     },
-    text:{
+    text: {
         color: 'white'
     },
     signup: {
@@ -139,4 +141,13 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         alignSelf: 'center',
     },
+    image: {
+        zIndex: 2,
+        width: 300,
+        height: 100,
+        alignSelf: 'center',
+    },
+    image_container: {
+        marginBottom: 120,
+    }
 });
